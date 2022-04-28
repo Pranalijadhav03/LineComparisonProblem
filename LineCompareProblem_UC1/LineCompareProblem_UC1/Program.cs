e5a6f2d66db832e882df5a0dd4a6a0d6310af54b
Pranalijadhav03 <jadhav.pranali2403@gmail.com>
2022-04-28 13:52:48 +0530

    Added: UC1

---
+++ LineCompareProblem_UC1/LineCompareProblem_UC1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to Line Comparison Computation Program");
+int x1 = 4, y1 = 5, x2 = 6, y2 = 7;
+double length = 0;
+length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+Console.WriteLine("length of the line = " + length);
+Console.ReadKey();
